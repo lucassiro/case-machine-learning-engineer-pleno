65d64a002e3134336d5ed1471378f8ceaa41b856
Lucas Siqueira <siqueiraq@gmail.com>
2024-08-26 07:52:13 -0300

    feat: implement tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,39 @@
+import json
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
+
 def test_health():
-    response = client.get("/health")
-    assert response.status_code == 200
+    response = client.get("/health/")
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {"status": "ok"}
+
+
+def test_history():
+    response = client.get("/model/history/")
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_load():
+    with open("../notebook/model.pkl", "rb") as f:
+        model = f.read()
+
+    files = {"model": model}
+    response = client.post(
+        url="/model/load/",
+        files=files,
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"status": "ok"}
+
+
+def test_predict():
+    response = client.post(
+        url="/model/predict/",
+        data=json.dumps({"origin_wind_speed": 1.0, "dest_wind_speed": 1.0})
+    )
+    assert response.status_code == HTTPStatus.OK
